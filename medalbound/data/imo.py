2d1014f90ef309984b1d0dddaeef7a2269a03e42
Joseph Myers <jsm@polyomino.org.uk>
2015-08-23 19:25:32 +0000

    Initial basic support for downloading and processing XML IMO results.

---
+++ medalbound/data/imo.py
@@ -0,0 +1,85 @@
+# IMOResults class for medalbound package.
+
+# Copyright 2015 Joseph Samuel Myers.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
+
+"""
+This module provides the IMOResults class that provides information
+about results of IMOs.
+"""
+
+import os.path
+import xml.etree.ElementTree
+
+from medalbound.data.results import Results
+
+__all__ = ['IMOResults']
+
+class IMOResults(Results):
+
+    """Subclass of Results providing IMO information."""
+
+    def get_cache_dir_name(self):
+        return 'imo-%d' % self.event_id
+
+    def get_cache_file_names(self):
+        return ['IMO_Individual.xml']
+
+    def get_data_url(self, file_name):
+        return ('https://www.imo-official.org/year_individual_r.aspx'
+                '?year=%d&column=total&order=desc&download=XML'
+                % self.event_id)
+
+    def imo_special_case(self, id):
+        """
+        Return whether this contestant should be excluded for the
+        purposes of medal boundaries consideration.
+        """
+        # At IMO 2005, because of problems with the transmission of
+        # one translation to the contestants, medal boundaries were
+        # determined without considering two contestants, and then
+        # those contestants were given awards as if they had scored 7
+        # on the affected problem.
+        if self.event_id != 2005:
+            return False
+        if id == '8678' or id == '8613':
+            return True
+        return False
+
+    def process_data(self):
+        xml_file_name = os.path.join(self.cache_dir, 'IMO_Individual.xml')
+        xml_et = xml.etree.ElementTree.parse(xml_file_name)
+        # This information isn't in the XML file, just assume it
+        # always has this value for years for which this code is used.
+        self.max_total = 42
+        self.total_stats = [0 for i in range(self.max_total + 1)]
+        award_stats = {'Gold medal': 0, 'Silver medal': 0, 'Bronze medal': 0,
+                       '': 0}
+        self.num_contestants = 0
+        for c in xml_et.iter('contestant'):
+            if not self.imo_special_case(c.attrib['id']):
+                self.num_contestants += 1
+                total = int(c.find('total').text)
+                award = c.find('award').text
+                if award == 'Honourable mention' or award is None:
+                    award = ''
+                if award not in award_stats:
+                    raise ValueError('Unknown award: %s' % award)
+                self.total_stats[total] += 1
+                award_stats[award] += 1
+        self.medal_stats = [award_stats['Gold medal'],
+                            award_stats['Silver medal'],
+                            award_stats['Bronze medal']]
---
+++ medalbound/data/results.py
@@ -0,0 +1,113 @@
+# Results class for medalbound package.
+
+# Copyright 2015 Joseph Samuel Myers.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
+
+"""
+This module provides the Results base class that provides information
+about results of mathematical olympiads.
+"""
+
+import os
+import os.path
+import sys
+_py3 = sys.version_info.major >= 3
+if _py3:
+    import urllib.request
+    urlretrieve = urllib.request.urlretrieve
+else:
+    import urllib
+    urlretrieve = urllib.urlretrieve
+
+__all__ = ['Results']
+
+class Results(object):
+
+    """
+    A Results object represents the information about the results of a
+    mathematical olympiad that is relevant to determining medal
+    boundaries, together with the actual medal boundaries at that
+    event.
+    """
+
+    def __init__(self, event_id):
+        """
+        Get the data for a given event, with an identifier that may be
+        an event number or year (whatever is convenient for that kind
+        of event).
+        """
+        self.event_id = event_id
+        self.cache_dir = os.path.join('cache', self.get_cache_dir_name())
+        if not os.access(self.cache_dir, os.F_OK):
+            os.makedirs(self.cache_dir)
+        files = self.get_cache_file_names()
+        for f in files:
+            if not os.access(os.path.join(self.cache_dir, f), os.F_OK):
+                self.download_data(f)
+        self.process_data()
+        self.set_cum_stats()
+
+    def get_cache_dir_name(self):
+        """
+        Return the name of a directory in which to cache event results.
+        """
+        raise NotImplementedError
+
+    def get_cache_file_names(self):
+        """Return a list of file names to download for an event."""
+        raise NotImplementedError
+
+    def download_data(self, file_name):
+        """
+        Download a particular file of data for an event.
+        """
+        url = self.get_data_url(file_name)
+        urlretrieve(url, os.path.join(self.cache_dir, file_name))
+
+    def get_data_url(self, file_name):
+        """
+        Return the URL to use for downloading a particular file of
+        data for an event.
+        """
+        raise NotImplementedError
+
+    def process_data(self):
+        """
+        Set various attributes of this object based on the data that
+        has been downloaded.  num_contestants is the number of
+        contestants.  max_total is the maximum total score of a
+        contestant (possibly not achieved).  total_stats has the
+        statistics of total scores.  medal_stats has the number of
+        each kind of medal (starting with gold), and is None if medal
+        boundaries have not yet been set at this event.
+        """
+        raise NotImplementedError
+
+    def set_cum_stats(self):
+        """Set the cumulative statistics of scores from this event."""
+        self.cum_total_stats = [0 for i in range(self.max_total + 1)]
+        cum_total = 0
+        for i in range(self.max_total, -1, -1):
+            cum_total += self.total_stats[i]
+            self.cum_total_stats[i] = cum_total
+        if self.medal_stats is None:
+            self.sum_medal_stats = None
+        else:
+            self.cum_medal_stats = [0 for i in range(len(self.medal_stats))]
+            cum_total = 0
+            for i in range(len(self.medal_stats)):
+                cum_total += self.medal_stats[i]
+                self.cum_medal_stats[i] = cum_total
