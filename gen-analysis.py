89addb3d52de37a30251756c95df5e94cc883d84
Joseph Myers <jsm@polyomino.org.uk>
2026-07-19 12:37:47 +0000

    Add analysis of unified boundary algorithms

--- gen-analysis.py
+++ gen-analysis.py
@@ -159,12 +159,65 @@ def gen_gs_alg_table(data):
         tex_file.write('\\end{tabular}\n')
 
 
+def gen_gsb_alg_table(data):
+    """
+    Generate list of years where unified algorithms fail to predict
+    the boundaries correctly.
+    """
+    algs = (('I', MedalAlgorithmIndependent()),
+            ('S', MedalAlgorithmLowestFirst()),
+            ('L^1', MedalAlgorithmLp(1, False)),
+            ('L^2', MedalAlgorithmLp(2, False)),
+            ('L^1s', MedalAlgorithmLp(1, True)),
+            ('L^2s', MedalAlgorithmLp(2, True)),
+            ('R', MedalAlgorithmRatio()))
+    with open('gen-gsb-alg-table.tex', 'w') as tex_file:
+        ydata = {}
+        for year in range(START_YEAR, NEXT_YEAR):
+            d = data[year]
+            dt = (d.cum_medal_stats[2], d.cum_medal_stats[1],
+                  d.cum_medal_stats[0])
+            ydata[year] = {dt: ['Actual']}
+        for alg in algs:
+            for year in range(START_YEAR, NEXT_YEAR):
+                d = data[year]
+                predicted = alg[1].compute_boundaries(d.cum_total_stats,
+                                                      [1, 2, 3, 6, 0],
+                                                      [None, None, None,
+                                                       d.cum_total_stats[0],
+                                                       None])
+                pt = (predicted[2], predicted[1], predicted[0])
+                if pt in ydata[year]:
+                    ydata[year][pt].append(alg[0])
+                else:
+                    ydata[year][pt] = [alg[0]]
+        max_num_opts = max([len(ydata[year].keys())
+                            for year in range(START_YEAR, NEXT_YEAR)])
+        col_descs = ['c' for i in range(max_num_opts+1)]
+        tex_file.write('\\begin{tabular}{|%s|}\n' % '|'.join(col_descs))
+        for year in range(START_YEAR, NEXT_YEAR):
+            yd = ydata[year]
+            ylist = []
+            for k in sorted(yd.keys()):
+                if 'Actual' in yd[k]:
+                    ylist.append('{\\tiny\\textbf{(%d, %d, %d)}}'
+                                 % (k[2], k[1]-k[2], k[0]-k[1]))
+                else:
+                    ylist.append('{\\tiny(%d, %d, %d)} {\\tiny $%s$}'
+                                 % (k[2], k[1]-k[2], k[0]-k[1],
+                                    ''.join(yd[k])))
+            ylist.extend(['' for i in range(max_num_opts - len(ylist))])
+            tex_file.write('%d & %s\\\\\n' % (year, ' & '.join(ylist)))
+        tex_file.write('\\end{tabular}\n')
+
+
 def main():
     """Generate all .tex files with IMO results analyses."""
     data = get_all_data()
     gen_bronze_table(data)
     gen_bronze_alg_list(data)
     gen_gs_alg_table(data)
+    gen_gsb_alg_table(data)
 
 
 main()
